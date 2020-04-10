751a0f882981acbb180f4bf364b6522fa07cccb0
Jimmy Zhao <zhao.jimmy@hotmail.com>
2020-04-09 20:21:00 -0400

    Remived some code.

--- script.py
+++ script.py
@@ -1,20 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-name =input("your name?")
-print("Hello,", name)
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
 
 
 r = requests.get('https://www.google.com/')
 print(r.status_code)
+print(r.ok)
